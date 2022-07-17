4da2f50c3ed6ddba2d0e3241b463d43093c3b8b5
h-orito <16453967+h-orito@users.noreply.github.com>
2022-07-17 17:55:22 +0900

    add available domain

--- src/main/kotlin/dev/wolfort/scenariotuker/domain/model/scenario/ScenarioUrl.kt
+++ src/main/kotlin/dev/wolfort/scenariotuker/domain/model/scenario/ScenarioUrl.kt
@@ -1,22 +1,34 @@
 package dev.wolfort.scenariotuker.domain.model.scenario
 
 import dev.wolfort.scenariotuker.fw.exception.SystemException
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 data class ScenarioUrl(
     val value: String
 ) {
-    private val availableDomains = listOf<String>(
-        "https://booth.pm/",
-        "https://talto.cc/",
-        "https://www.pixiv.net/",
-        "http://character-sheets.appspot.com/",
-        "https://character-sheets.appspot.com/",
-        "https://twitter.com/"
+    private val availableDomains = listOf(
+        "booth.pm",
+        "talto.cc",
+        "www.pixiv.net",
+        "character-sheets.appspot.com",
+        "twitter.com",
+        "www.dlsite.com"
     )
 
     fun assert() {
-        if (!availableDomains.contains(value)) {
+        val domain = getUrlDomain() ?: throw SystemException("許可されていないURLです。 url: $value")
+        if (availableDomains.none { domain.endsWith(it) }) {
             throw SystemException("許可されていないURLです。 url: $value")
+        }
+    }
+
+    private fun getUrlDomain(): String? {
+        val m: Matcher = Pattern.compile("https?://([^/]+)/").matcher(value)
+        return if (m.find()) {
+            m.group(1)
+        } else {
+            null
         }
     }
 }
